a75d8c4213e3138c1ea37fd2716492acfcbd2d19
simranjit-kaur822 <simikaur.work@gmail.com>
2025-12-27 12:38:08 +0530

    add greeting message to App component

--- logowebsite/src/App.jsx
+++ logowebsite/src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <>
+      <p className='bg-blue-900 text-white text-2xl font-bold text-center'>Hello world</p>
+    </>
   )
 }
 
