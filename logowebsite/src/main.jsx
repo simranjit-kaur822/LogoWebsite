f1d5e88734119a2f1a9283233df99a65139bd4e3
simranjit-kaur822 <simikaur.work@gmail.com>
2026-01-01 17:31:13 +0530

    add Contact, User, and Github components to routing in main.jsx

--- logowebsite/src/main.jsx
+++ logowebsite/src/main.jsx
@@ -6,6 +6,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/ContactUs/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 
 const router = createBrowserRouter(
   [
@@ -20,6 +23,18 @@ const router = createBrowserRouter(
         {
           path: "about",
           element: <About/>
+        },
+        {
+          path: "contact",
+          element: <Contact/>
+        },
+        {
+          path: "user/:userId",
+          element: <User/>
+        },
+        {
+          path: "github",
+          element: <Github/>
         }
       ]
     }
